ab06ccaac2c0db99ebc2e9410ef7685010da8986
SaranshG2501 <saranshg180@gmail.com>
2025-03-25 13:51:05 +0530

    tailwind update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
