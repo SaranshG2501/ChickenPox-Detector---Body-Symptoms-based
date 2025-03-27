c774243343e1669959f7a1e7d6821647d57b2901
SaranshG2501 <saranshg180@gmail.com>
2025-03-27 10:04:01 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -22,8 +23,8 @@ export default defineConfig(({ mode }) => ({
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        tailwindcss,
+        autoprefixer,
       ],
     },
   },
