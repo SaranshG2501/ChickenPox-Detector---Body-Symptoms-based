2488fb1276953520e76a9a314ee9ef6e3a789a6a
SaranshG2501 <saranshg180@gmail.com>
2025-04-01 09:50:33 +0530

    index pages

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -26,7 +26,7 @@ const LogoutButton = () => {
       className={`flex items-center justify-center text-gray-800 hover:text-black border-gray-300 hover:bg-gray-200 transition-colors h-8 sm:h-9 ${isMobile ? 'px-2' : 'px-3'}`}
     >
       <LogOut className={`h-4 w-4 ${!isMobile ? 'mr-0 sm:mr-2' : ''}`} />
-      {!isMobile && <span className="hidden sm:inline">Logout</span>}
+      {!isMobile && <span className=" sm:inline">Logout</span>}
       {isMobile && <span className="sr-only">Logout</span>}
     </Button>
   );
